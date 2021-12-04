fb125ee88b0e1d8d241c90eccd22213dabff3e3a
Jazmine Abdilahi <66798977+Jazmine-Abdilahi@users.noreply.github.com>
2021-12-04 13:17:10 -0600

    Add files via upload

---
+++ Dec4.js
@@ -0,0 +1,25 @@
+let js = "amazing"
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jazmine');
+console.log(11);
+
+let firstName = "Yasmine";
+
+console.log(firstname);
+console.log(firstname);
+console.log(firstname);
+
+let Jazmine_yazmine = "jy"
+let $function = 27;
+
+let person = "Yasmine"
+let PI = 3.1415;
+
+let myFrstJob = "programmer";
+let myCurrentJob = "Student";
+
+let job1 = "programmer";
+let job2 = "student";
+
+console.log(myFirstJob)
